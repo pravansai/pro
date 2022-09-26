85b2e79937a80cd6f7de57fd95f659d9ccea0251
root <root@ip-172-31-82-57.ec2.internal>
2022-09-26 13:02:06 +0000

    first

--- main.tf
+++ main.tf
@@ -9,23 +9,6 @@ key_name = "pro"
 tags = {
 name = "mymachine"
 }
-ingress {
-from_port = 8080
-to_port = 8080
-protocol = "tcp"
-cidr_block = ["0.0.0.0/0"]
-}
-ingress {
-from_port = 22
-to_port = 22
-protocol = "tcp"
-cidr_block = ["0.0.0.0/0"]
-}
-egress {
-from_port = 0
-to_port = 65535
-protocol = "tcp"
-cidr_block = ["0.0.0.0/0"]
 }
 resource "aws_eip" "ec2" {
 vpc = true
