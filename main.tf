5a0d966f16dc096295ce69625700c85e9cb8e5e6
root <root@ip-172-31-82-57.ec2.internal>
2022-09-26 11:17:06 +0000

    first

--- main.tf
+++ main.tf
@@ -6,10 +6,28 @@ resource "aws_instance" "ec2" {
 ami = "ami-026b57f3c383c2eec"
 instance_type = "t2.micro"
 key_name = "pro"
+tags = {
+name = "mymachine"
 }
-resource "aws_ami_from_instance" "example" {
-  name               = "terraform"
-  source_instance_id = "$amicreation"
+ingress {
+from_port = 8080
+to_port = 8080
+protocol = "tcp"
+cidr_block = ["0.0.0.0/0"]
 }
-
-
+ingress {
+from_port = 22
+to_port = 22
+protocol = "tcp"
+cidr_block = ["0.0.0.0/0"]
+}
+egress {
+from_port = 0
+to_port = 65535
+protocol = "tcp"
+cidr_block = ["0.0.0.0/0"]
+}
+resource "aws_eip" "ec2" {
+vpc = true
+instance = aws_instance.ec2.id
+}
