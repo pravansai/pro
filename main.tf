b678c8e6efa9fa2ac7963b227a9e2f895d78e143
root <root@ip-172-31-82-57.ec2.internal>
2022-09-26 10:21:01 +0000

    first

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+profile = "default"
+region = "us-east-1"
+}
+resource "aws_instance" "ec2" {
+ami = "ami-026b57f3c383c2eec"
+instance_type = "t2.micro"
+key_name = "pro"
+}
+resource "aws_ami_from_instance" "example" {
+  name               = "terraform"
+  source_instance_id = "i-05163207af2a98ebd"
+}
+
+
